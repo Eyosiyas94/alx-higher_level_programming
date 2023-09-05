77411d5f7c2c56a9cf4b9d0d8ee744373056cb2a
Eyosiyas94 <134216719+Eyosiyas94@users.noreply.github.com>
2023-09-05 19:06:36 +0300

    gu

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(26):
-    if i % 2 == 0:
-        print('{:c}'.format(122 - i), end='')
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        print("{:c}".format(i - 32), end='')
     else:
-        print('{:c}'.format(90 - i), end='')
+        print("{:c}".format(i), end='')
