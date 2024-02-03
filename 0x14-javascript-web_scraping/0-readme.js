4dcb5e9d2bb4c62731275d23cfccd2a196175fb0
Eyosiyas94 <134216719+Eyosiyas94@users.noreply.github.com>
2024-02-03 22:12:19 +0300

    as

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
+
+const fileName = process.argv[2];
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+fs.readFile(fileName, 'utf-8', (err, data) => {
   if (err) {
-    console.log(err);
-  } else {
-    console.log(data.toString());
+    console.error(err);
   }
+  console.log(data);
 });
