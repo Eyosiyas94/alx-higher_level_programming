a4a7c1aadfb6ef72df5926eec2f51360640d79f5
Eyosiyas94 <134216719+Eyosiyas94@users.noreply.github.com>
2024-02-06 21:11:49 +0300

    d

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
-// A script that prints the title of a Star Wars movie where the episode num matches a given int
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.log(error);
   } else {
-    console.log(JSON.parse(body).title);
+    const data = JSON.parse(body);
+    console.log(data.title);
   }
 });
