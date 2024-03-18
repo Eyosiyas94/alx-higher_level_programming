5c62e5c322031d2cd719668bcfcb98be4895983c
Eyosiyas94 <134216719+Eyosiyas94@users.noreply.github.com>
2024-03-18 22:01:26 +0300

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-function factorial (num) {
-  if (num === 1) {
-    return 1;
+function factorial (n) {
+  if (n > 0) {
+    return (n * factorial(n - 1));
+  } else if (n === 0) {
+    return (1);
   } else {
-    return num * factorial(num - 1);
+    return (1);
   }
 }
-
-if (isNaN(process.argv[2])) {
-  console.log(1);
-} else {
-  console.log(factorial(parseInt(process.argv[2])));
-}
+const number = Number(process.argv[2]);
+console.log(factorial(number));
